1dbe248c60a4723a39c9fbe86272b761e2503817
EvgeniyTLT <Toy444@ya.ru>
2022-07-26 19:09:01 +0300

    Add

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,21 +5,33 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    val counter = 5
+    lateinit var tv: TextView
+    lateinit var bt: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv = findViewById(R.id.tvTest)
+        tv.text="1111"
+        bt = findViewById(R.id.bTest)
+        bt.setOnClickListener{
+            tv.text = "2222"
+        }
+
+
 
     }
 
-    fun onClickTest(view: View) {
-        val tvTest = findViewById<TextView>(R.id.tvTest)
-        if (tvTest.text.equals("privet")) tvTest.text = "NOTprivet"
-        else tvTest.text = "privet"
+    override fun onPause() {
+        super.onPause()
 
     }
+
+
+
+
 }
