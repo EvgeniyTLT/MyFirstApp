a1eb773a63f0fc7d90f636f2d3aa71a24e73d00e
EvgeniyTLT <Toy444@ya.ru>
2022-07-27 10:45:02 +0300

    Add

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,34 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tv: TextView
-    lateinit var bt: Button
+    lateinit var bindingClass: ActivityMainBinding
+    val a = 324
+    val b = 34
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        tv = findViewById(R.id.tvTest)
-        tv.text="1111"
-        bt = findViewById(R.id.bTest)
-        bt.setOnClickListener{
-            tv.text = "2222"
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.b1.setOnClickListener {
+            val result = a + b
+            bindingClass.tvResult.text = "Результат сложения равен: $result"
+        }
+        bindingClass.b2.setOnClickListener {
+            val result = a - b
+            bindingClass.tvResult.text = "Результат вычитания равен: $result"
+        }
+        bindingClass.b3.setOnClickListener {
+            val result = a * b
+            bindingClass.tvResult.text = "Результат умножения равен: $result"
         }
 
-
-
     }
-
-    override fun onPause() {
-        super.onPause()
-
-    }
-
-
-
-
 }
