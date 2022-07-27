cfa43438bd307ef7e4a458786e97b2494e91d0e4
EvgeniyTLT <Toy444@ya.ru>
2022-07-28 00:26:31 +0300

    Add

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,38 +1,45 @@
 package com.example.myfirstapp
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import com.example.myfirstapp.constance.Constance
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
         bindingClass.btResult.setOnClickListener {
-            val result = bindingClass.edValue.text.toString().toInt()
+            val result = bindingClass.edValue.text.toString()
+
             when (result) {
-                in 1..1000 -> {
+                Constance.INGINER -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Начинающих блогер"
+                    val tempText = "Получите ваши ${Constance.INGINER_ZP} рублей"
+                    bindingClass.tvResult.text =
+                        if (bindingClass.edPassword.text.toString() == Constance.INGINER_PASSWORD) {
+                            tempText
+                        } else {
+                            "Введен не верный пароль"
+                        }
                 }
-                in 1000..100000 -> {
+                Constance.DIRECTOR -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Средний блогер"
+                    val tempText = "Получите ваши ${Constance.DIRECTOR_ZP} рублей"
+                    bindingClass.tvResult.text =
+                        if (bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD) {
+                            tempText
+                        } else {
+                            "Введен не верный пароль"
+                        }
                 }
                 else -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Вы звезда"
+                    bindingClass.tvResult.text = "Нет такого рабочего"
                 }
             }
         }
---
+++ app/src/main/java/com/example/myfirstapp/constance/Constance.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstapp.constance
+
+object Constance {
+    const val INGINER_ZP = 5000
+    const val DIRECTOR_ZP = 100000
+
+    const val DIRECTOR = "Egor"
+    const val INGINER = "Sergey"
+
+    const val INGINER_PASSWORD = "5656"
+    const val DIRECTOR_PASSWORD = "0101"
+}
