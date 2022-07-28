2e8c4d74155d2df03a84ddef77dc6fccca478c7b
EvgeniyTLT <Toy444@ya.ru>
2022-07-29 01:40:03 +0300

    Add

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.myfirstapp.constance.Constance
+
 import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,4 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            bindingClass.textView4.text = data.getStringExtra("key2")
+        }
+    }
+
+    fun onClickTest1(view: View) {
+        val i = Intent(this, TestActivity1::class.java)
+        i.putExtra("key", "Как тебя зовут?")
+        startActivityForResult(i, 100)
+    }
+
 }
---
+++ app/src/main/java/com/example/myfirstapp/TestActivity1.kt
@@ -0,0 +1,26 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.myfirstapp.databinding.ActivityTest1Binding
+
+class TestActivity1 : AppCompatActivity() {
+    lateinit var bindingClass: ActivityTest1Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityTest1Binding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        val message = intent.getStringExtra("key")
+        bindingClass.tvMessage.text = message
+    }
+
+    fun onClickBack(view: View) {
+        val i = Intent(this, TestActivity1::class.java)
+        i.putExtra("key2", bindingClass.edName.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+}
