2f128b2eb95ab36520f078d394b94abceba5ff66
EvgeniyTLT <Toy444@ya.ru>
2022-07-31 23:18:51 +0300

    Add

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,82 +1,25 @@
 package com.example.myfirstapp
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.view.isVisible
-import com.example.myfirstapp.constance.Constance
+import android.util.Log
 
-import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass: ActivityMainBinding
-    private var login: String = "empty"
-    private var password: String = "empty"
-    private var name: String = "empty"
-    private var name2: String = "empty"
-    private var name3: String = "empty"
-    private var avatarImageId: Int = 0
+
+    val lostArray = arrayOf(10000, 2300, 45000, 65000, 6500, 400)
+    val earnArray = arrayOf(15000, 300, 345000, 5000, 16500, 3400)
+    val resultArray = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
+        val names = resources.getStringArray(R.array.names)
+        for ((index, name) in names.withIndex()) {
+            resultArray.add("Имя: $name - прибыль: ${earnArray[index] - lostArray[index]}")
+            Log.d("MyLog", "Статистика -/- ${resultArray[index]}")
 
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == Constance.REQUEST_COD_SING_IN) {
-            val l = data?.getStringExtra(Constance.LOGIN)
-            val p = data?.getStringExtra(Constance.PASSWORD)
-            if (login == l && password == p) {
-                bindingClass.imAvatar.visibility = View.VISIBLE
-                bindingClass.imAvatar.setImageResource(avatarImageId)
-                val textInfo = "$name $name2 $name3"
-                bindingClass.tvInfo.text = textInfo
-                bindingClass.bHide.visibility = View.GONE
-                bindingClass.bExit.text = "Выйти"
-            } else {
-                bindingClass.imAvatar.visibility = View.VISIBLE
-                bindingClass.tvInfo.text = "Такого аккаунта не существует"
-            }
-
-
-        } else if (requestCode == Constance.REQUEST_COD_SING_UP) {
-            login = data?.getStringExtra(Constance.LOGIN)!!
-            password = data?.getStringExtra(Constance.PASSWORD)!!
-            name = data?.getStringExtra(Constance.NAME)!!
-            name2 = data?.getStringExtra(Constance.NAME2)!!
-            name3 = data?.getStringExtra(Constance.NAME3)!!
-            avatarImageId = data?.getIntExtra(Constance.AVATAR_ID, 0)!!
-            bindingClass.imAvatar.visibility = View.VISIBLE
-            bindingClass.imAvatar.setImageResource(avatarImageId)
-            val textInfo = "$name $name2 $name3"
-            bindingClass.tvInfo.text = textInfo
-            bindingClass.bHide.visibility = View.GONE
-            bindingClass.bExit.text = "Выйти"
         }
     }
-
-    fun onClickSingIn(view: View) {
-
-        if (bindingClass.imAvatar.isVisible && bindingClass.tvInfo.text.toString() != "Такого аккаунта не существует") {
-            bindingClass.imAvatar.visibility = View.INVISIBLE
-            bindingClass.tvInfo.text = ""
-            bindingClass.bHide.visibility = View.VISIBLE
-            bindingClass.bExit.text = getString(R.string.sign_in)
-        } else {
-            val intent = Intent(this, SignInUpAct::class.java)
-            intent.putExtra(Constance.SING_STATE, Constance.SING_IN_STATE)
-            startActivityForResult(intent, Constance.REQUEST_COD_SING_IN)
-        }
-    }
-
-    fun onClickSingUp(view: View) {
-        val intent = Intent(this, SignInUpAct::class.java)
-        intent.putExtra(Constance.SING_STATE, Constance.SING_UP_STATE)
-        startActivityForResult(intent, Constance.REQUEST_COD_SING_UP)
-    }
-
 }
